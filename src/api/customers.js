d0afbaebc8d1ca860322f3aa5141dd6c250b9ec3
Yihan Lu <yihanlu@Yihans-MacBook.local>
2018-06-17 14:22:58 +1000

    finish off customers API

--- src/api/customers.js
+++ src/api/customers.js
@@ -2,55 +2,87 @@ import resource from 'resource-router-middleware';
 import fs from 'fs';
 //import customers from '../../data/customers.json';
 
+const NOT_FOUND_ERROR_MSG = 'Customer not found.';
+const CUSTOMERS_FILE_PATH = './customers.json';
+
 export default ({ config, db }) => resource({
 
 	/** Property name to store preloaded entity on `request`. */
-	id : 'facet',
+	id : 'customer',
 
 	/** For requests with an `id`, you can auto-load the entity.
 	 *  Errors terminate the request, success sets `req[id] = data`.
 	 */
 	load(req, id, callback) {
-		let facet = facets.find( facet => facet.id===id ),
-			err = facet ? null : 'Not found';
-		callback(err, facet);
+        const {data, error} = getCustomerData();
+        if(!error) {
+            customer = data;
+        } else {
+            err = error
+        }
+		let customer = data.find( a => a.id == id ),
+			err = customer ? null : NOT_FOUND_ERROR_MSG;
+		callback(err, customer);
 	},
 
 	/** GET / - List all entities */
-	index({ params }, res) {
-        try {
-            const data = fs.readFileSync('./customers.json', 'utf8');
-            return res.json(JSON.parse(data))
-        } catch (err) {
-            return res.boom.badRequest("Error reading or parsing json file.", err)
+	index({ params, query: { term } }, res) {
+        let customers = []
+        const { data, error } = getCustomerData();
+        if(!error) {
+            customers = data;
+        } else {
+            return res.boom.badRequest( error )
         }
-	},
-
-	/** POST / - Create a new entity */
-	create({ body }, res) {
-		body.id = facets.length.toString(36);
-		facets.push(body);
-		res.json(body);
-	},
-
-	/** GET /:id - Return a given entity */
-	read({ facet }, res) {
-		res.json(facet);
-	},
-
+        if (term) {
+            customers = customers.filter( a => ( a.name.includes(term) || a.email.includes(term)))
+        }
+        return customers.length > 0 ? res.json(customers) : res.boom.notFound( NOT_FOUND_ERROR_MSG )
+    },
+    
 	/** PUT /:id - Update a given entity */
-	update({ facet, body }, res) {
+	update({ customer, body }, res) {
+        const { data, error } = getCustomerData()
+        if(body.hasOwnProperty('email') && !body.email) {
+            return res.boom.badRequest( 'Email can not be empty' );
+        } else if(body.hasOwnProperty('name') && !body.name) {
+            return res.boom.badRequest( 'Name can not be empty' );
+        }
 		for (let key in body) {
 			if (key!=='id') {
-				facet[key] = body[key];
+				customer[key] = body[key];
 			}
-		}
-		res.sendStatus(204);
+        }
+        data[customer.id-1] = customer;
+        try {
+            fs.writeFileSync(CUSTOMERS_FILE_PATH, JSON.stringify(data))
+            return res.sendStatus(204)
+        } catch ( err ) {
+            return res.boom.badRequest('Error writing to the file.');
+        }
 	},
 
 	/** DELETE /:id - Delete a given entity */
 	delete({ facet }, res) {
 		facets.splice(facets.indexOf(facet), 1);
 		res.sendStatus(204);
-	}
+    }
+
 });
+
+const getCustomerData = () => {
+    let customers = {
+        error: false,
+        data: []
+    }
+    try {
+        customers.data = JSON.parse(fs.readFileSync(CUSTOMERS_FILE_PATH, 'utf8'));
+        if(!Array.isArray(customers.data)) {
+            throw new Error('Invalid customer data.');
+        }
+        return customers;
+    } catch (err) {
+        customers.error = err;
+        return customers;
+    }
+}
